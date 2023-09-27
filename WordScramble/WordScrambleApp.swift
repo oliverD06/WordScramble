143d554abd3927f5c8e1aa6c9eca61d78e9423fa
Oliver Delaney <oliver.delaney@stu.barren.kyschools.us>
2023-09-27 09:57:13 -0500

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Oliver Delaney on 9/27/23.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
